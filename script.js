f2800b355d5d72c116af0d0f6d22d78fc2028994
Addam Benson <abenson9@cnm.edu>
2022-07-05 12:47:54 -0700

    Change 1 - Span value not responding

---
+++ script.js
@@ -0,0 +1,44 @@
+window.onload = function () {
+    initColorPicker();
+};
+
+function initColorPicker() {
+    let colorBox = document.getElementById("color-box")
+    let rgb = {
+        red: document.getElementById("red"),
+        green: document.getElementById("green"),
+        blue: document.getElementById("blue"),
+    };
+    let colorPickers = document.getElementsByClassName("picker");
+    setColorPickerEventListeners(colorBox, rgb, colorPickers);
+}
+
+function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
+    let pickerLen = pickerElements.length;
+    for (let i = 0; i < pickerLen; i++) {
+        pickerElements[i].addEventListener('change', () => {
+            let red = rgb.red.value;
+            let green = rgb.green.value;
+            let blue = rgb.blue.value;
+            setBoxBGColor(colorBox, red, green, blue)
+            setDisplayValues(red, green, blue)
+        });
+    }
+}
+
+
+function setBoxBGColor(colorBox, red, green, blue) {
+    let rgbVal = [red, green, blue].join(',');
+    colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
+}
+
+function setDisplayValues(red, green, blue) {
+    let redVal = document.getElementById("redVal");
+    let greenVal = document.getElementById("greenVal");
+    let blueVal = document.getElementById("blueVal");
+
+    redVal.innertext = red;
+    greenVal.innertext = green;
+    blueVal.innertext = blue;
+}
+
