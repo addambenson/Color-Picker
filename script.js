07142f39bf9a19465f264a1247c4efa3a791cc91
Addam Benson <abenson9@cnm.edu>
2022-07-06 09:05:38 -0700

    Change 2 -Assignment complete

--- script.js
+++ script.js
@@ -3,31 +3,31 @@ window.onload = function () {
 };
 
 function initColorPicker() {
-    let colorBox = document.getElementById("color-box")
+    let element = document.getElementById("color-box")
     let rgb = {
         red: document.getElementById("red"),
         green: document.getElementById("green"),
         blue: document.getElementById("blue"),
     };
     let colorPickers = document.getElementsByClassName("picker");
-    setColorPickerEventListeners(colorBox, rgb, colorPickers);
+    setColorPickerEventListeners(element, rgb, colorPickers);
 }
 
-function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
+function setColorPickerEventListeners(element, colors, pickerElements) {
     let pickerLen = pickerElements.length;
     for (let i = 0; i < pickerLen; i++) {
         pickerElements[i].addEventListener('change', () => {
-            let red = rgb.red.value;
-            let green = rgb.green.value;
-            let blue = rgb.blue.value;
-            setBoxBGColor(colorBox, red, green, blue)
+            let red = colors.red.value;
+            let green = colors.green.value;
+            let blue = colors.blue.value;
+            setElementBGColor(element, red, green, blue)
             setDisplayValues(red, green, blue)
         });
     }
 }
 
 
-function setBoxBGColor(colorBox, red, green, blue) {
+function setElementBGColor(colorBox, red, green, blue) {
     let rgbVal = [red, green, blue].join(',');
     colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
 }
